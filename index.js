1cec0a117363179bd87f5e913569527d8fac39fe
NathanTan <tann@oregonstate.edu>
2019-02-12 15:16:56 -0800

    Example is currently working.

---
+++ index.js
@@ -0,0 +1,20 @@
+var assert = require("assert")
+var ChessState = require("chessstate")
+
+// Initalize a new game
+let game = new ChessState.ChessState()
+
+// Execute move "e4" for white.
+game.move("e4")
+
+// Execute move "e5" for black.
+game.move("e5")
+
+// Print out an ascii representation of the board.
+game.printBoard()
+
+// Print out the FEN string representing the game.
+console.log(game.getFen())
+
+// Print out game state object.
+console.log(game.getStatus())
